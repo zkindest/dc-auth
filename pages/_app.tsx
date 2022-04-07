03a200a47dca3c4ee60eefca5fb3f997645c6122
zkindest <zkindest5@gmail.com>
2022-04-07 18:14:28 +0530

    load initial user synchronously before browser repaint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import { AppProps } from "next/app"
 import { ThemeProvider } from "../lib/theme"
 import "../styles/base.css"
 
-import { useEffect } from "react"
+import { useEffect, useLayoutEffect, useState } from "react"
 import { getAuthFetchOptions } from "~/utils/client/auth"
 import { useAtom } from "jotai"
 import { userAtom } from "~/jotai/user"
@@ -10,8 +10,30 @@ import ErrorBoundary from "~/components/ErrorBoundary"
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const [_, setUser] = useAtom(userAtom)
+  const [loadUser, setLoadUser] = useState(false)
+
+  // Wait until after client-side hydration to show
   useEffect(() => {
+    setLoadUser(true)
+  }, [])
+
+  return (
+    <ThemeProvider>
+      <ErrorBoundary>
+        {loadUser && <LoadInitialUser />}
+        <Component {...pageProps} />
+      </ErrorBoundary>
+    </ThemeProvider>
+  )
+}
+
+const LoadInitialUser = () => {
+  const [_, setUser] = useAtom(userAtom)
+
+  useLayoutEffect(() => {
+    // to avoid state updates after component unmount
+    // see: https://stackoverflow.com/questions/53949393/cant-perform-a-react-state-update-on-an-unmounted-component
+    let isActive = true
     try {
       ;(async () => {
         const result = await (
@@ -20,17 +42,14 @@ export default function MyApp({ Component, pageProps }: AppProps) {
             ...getAuthFetchOptions(),
           })
         ).json()
-        setUser(result.data)
+        if (isActive) setUser(result.data)
       })()
     } catch (err) {
       console.error(err)
     }
+    return () => {
+      isActive = false
+    }
   }, [setUser])
-  return (
-    <ThemeProvider>
-      <ErrorBoundary>
-        <Component {...pageProps} />
-      </ErrorBoundary>
-    </ThemeProvider>
-  )
+  return <></>
 }
