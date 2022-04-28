31f650ef0deecbd0e337e00c3b945bdb75a6c3b7
zkindest <zkindest5@gmail.com>
2022-04-28 20:38:19 +0530

    modify pre-build script to avoid vercel bot builds\ on renovate bot PRs

--- scripts/pre-build.sh
+++ scripts/pre-build.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-echo "checking git commit message for possible \`[skip ci]\`"
-echo "VERCEL_GIT_COMMIT_MESSAGE: $VERCEL_GIT_COMMIT_MESSAGE"
 
-if [[ "$VERCEL_GIT_COMMIT_MESSAGE" == *"[skip ci]"* ]] ; then
+echo "VERCEL_GIT_COMMIT_MESSAGE: $VERCEL_GIT_COMMIT_MESSAGE"
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+if [[  "$VERCEL_GIT_COMMIT_REF" == "renovate"*  ]]; then
+  # Don't build
+  echo "🛑 - Build skipped. All \`renovate\` will not be deployed."
+  echo "Check scripts/pre-build.sh for more info."
+  exit 0;
+elif [[ "$VERCEL_GIT_COMMIT_MESSAGE" == *"[skip ci]"* ]] ; then
   # Don't build
   echo "🛑 - Build skipped due to \`[skip ci]\` in commit message."
+  echo "Check scripts/pre-build.sh for more info."
   exit 0;
-
 else
   # Proceed with the build
   echo "✅ - Build can proceed"
